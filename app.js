0a320d2dfe73b40783fca8a9f61d4d2ad4f5b6c9
AndresSapriza <andresapriza@hotmail.com>
2021-03-03 23:55:55 -0300

    allow any origins

--- app.js
+++ app.js
@@ -13,22 +13,21 @@ const app = express();
 
 const port = process.env.PORT || 3002;
 
-app.use(cors());
 // const allowedOrigins = [process.env.ALLOWEDORIGINS];
 
-// app.use(cors({
-// 	origin(origin, callback) {
-// 		// allow requests with no origin
-// 		// (like mobile apps or curl requests)
-// 		if (!origin) return callback(null, true);
-// 		if (allowedOrigins.indexOf(origin) === -1) {
-// 			const msg = 'The CORS policy for this site does not '
-//                 + 'allow access from the specified Origin.';
-// 			return callback(new Error(msg), false);
-// 		}
-// 		return callback(null, true);
-// 	},
-// }));
+app.use(cors({
+	origin(origin, callback) {
+		// allow requests with no origin
+		// (like mobile apps or curl requests)
+		if (!origin) return callback(null, true);
+		// if (allowedOrigins.indexOf(origin) === -1) {
+		// 	const msg = 'The CORS policy for this site does not '
+        //         + 'allow access from the specified Origin.';
+		// 	return callback(new Error(msg), false);
+		// }
+		return callback(null, true);
+	},
+}));
 
 app.use(morgan('tiny'));
 app.use(express.json());
